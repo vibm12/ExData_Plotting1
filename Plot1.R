b36ac48f9b3f3110338d13fc859ae8135364592d
vaibhav mahajan <49052344+vibm12@users.noreply.github.com>
2020-06-10 11:27:36 +0530

    Create Plot1.R

---
+++ Plot1.R
@@ -0,0 +1,16 @@
+# make sure the plots folder exists
+if (!file.exists('plots')) {
+  dir.create('plots')
+}
+
+# load data
+source('scripts/get_and_clean_data.R')
+
+# open device
+png(filename='plots/plot1.png',width=480,height=480,units='px')
+
+# plot data
+hist(power.consumption$GlobalActivePower,main='Global Active Power',xlab='Global Active Power (kilowatts)',col='red')
+
+# Turn off device
+x<-dev.off()
